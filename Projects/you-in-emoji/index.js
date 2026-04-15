fe08de6280e1d36c935bf7a98b42e81bcc495d33
rahuldascse18 <rahuldas18@cse.pstu.ac.bd>
2026-04-15 14:42:48 +0600

    From local storage

--- Projects/you-in-emoji/index.js
+++ Projects/you-in-emoji/index.js
@@ -1,17 +1,12 @@
 
-
-const emoji = ["👨🏼‍💻", "🌱", "🍲"]
-
+let emoji = ["👨🏼‍💻", "🌱", "🍲"]
 let emojiContainer = document.getElementById("emoji-container");
-
-
 let heading = ""
 for(let i = 0; i < emoji.length; i++) {
     heading += `
     <span> ${emoji[i]} </span>
     `
 }
-
 emojiContainer.innerHTML = heading;
 
 
@@ -22,36 +17,36 @@ let removeStart= document.getElementById("remove-start");
 let removeEnd = document.getElementById("remove-end");
 
 
-
-
-
+////////////////////Four functional Button/////////////////////////////
 addStart.addEventListener("click", function(){
     if(inputEmoji.value){
         emoji.unshift(inputEmoji.value)
+        localStorage.setItem("myEmoji", JSON.stringify(emoji))
         inputEmoji.value = ""
         renderEmoji();
     }
 })
-
 addEnd.addEventListener("click", function(){
     if(inputEmoji.value){                 //if(inputEmoji.value)
         emoji.push(inputEmoji.value)
+        localStorage.setItem("myEmoji", JSON.stringify(emoji))
         inputEmoji.value = ""
         renderEmoji();
     }
 })
-
 removeStart.addEventListener("click", function(){
     emoji.shift();
+    localStorage.setItem("myEmoji", JSON.stringify(emoji))
     renderEmoji();
 })
-
 removeEnd.addEventListener("click", function(){
     emoji.pop();
+    localStorage.setItem("myEmoji", JSON.stringify(emoji))
     renderEmoji();
 })
 
 
+emoji = JSON.parse(localStorage.getItem("myEmoji")) || []
 
 function renderEmoji() {
     heading = "";
@@ -63,5 +58,8 @@ function renderEmoji() {
     emojiContainer.innerHTML = heading;
 }
 
+console.log(emoji);
 
-
+window.addEventListener("load", function(){
+    renderEmoji();
+})
